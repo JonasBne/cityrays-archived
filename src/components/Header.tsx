2f0e7002937f910800838c9432fe60e45081f130
Jonas Boone <boone.jonas@outlook.be>
2023-03-26 14:11:31 +0200

    refactor(Header): add login/log out functionality and version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,28 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 
+const version = process.env.NEXT_PUBLIC_APP_VERSION;
+
 export const Header = () => {
   const { data: session } = useSession();
 
+  const handleClick = () => {
+    if (!session) {
+      return void signIn();
+    }
+    return void signOut();
+  };
+
   return (
-    <header className="flex items-center justify-between px-4 py-2">
-      <h1>CityRays</h1>
+    <header className="flex items-center justify-between p-4">
+      <h1 className="text-xl font-bold tracking-wider">CityRays</h1>
       <div>
-        {session ? (
-          <>
-            <span>{session.user.name}</span>
-            <button onClick={() => void signOut()}>Log out</button>
-          </>
-        ) : (
-          <button onClick={() => void signIn()}>Sign in</button>
-        )}
+        <button
+          className="mr-2 rounded-md bg-slate-600 px-2 py-1 text-sm"
+          onClick={handleClick}
+        >
+          {session ? "Log out" : "Log in"}
+        </button>
+        {version && <span className="text-sm font-light">{`v${version}`}</span>}
       </div>
     </header>
   );
