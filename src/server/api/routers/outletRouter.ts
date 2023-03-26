422dd01345b58c9181b1c6f00272f784ff8736ee
Jonas Boone <boone.jonas@outlook.be>
2023-03-26 16:52:45 +0200

    feat(getAllOpen): add query to get all open outlets

--- src/server/api/routers/outletRouter.ts
+++ src/server/api/routers/outletRouter.ts
@@ -18,6 +18,7 @@ export const outletRouter = createTRPCRouter({
         where: {
           AND: [
             {
+              // find outlets that are open on the current weekday
               openingHours: {
                 some: { weekday: currentWeekday.toLowerCase() },
               },
@@ -26,9 +27,12 @@ export const outletRouter = createTRPCRouter({
               openingHours: {
                 some: {
                   OR: [
+                    // the outlet is open if the closesAt time is greater than the current time
                     {
                       closesAt: { gt: currentTime },
                     },
+                    // or the outlet is open if it closes on the next day (after midnight) and the
+                    // current time is lower than the closesAt time
                     {
                       closesAt: { lt: currentTime },
                       closesAtNextDay: true,
