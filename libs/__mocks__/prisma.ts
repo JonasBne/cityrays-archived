429c87c5d271bc520b50c529db55aee25013a142
Jonas Boone <boone.jonas@outlook.be>
2023-04-12 14:24:24 +0200

    test(outletRouter): getAll WIP

--- libs/__mocks__/prisma.ts
+++ libs/__mocks__/prisma.ts
@@ -5,9 +5,8 @@ import { mockDeep, mockReset } from "vitest-mock-extended";
 
 // reset the prisma client mock between each individual test
 beforeEach(() => {
-  mockReset(prisma);
+  mockReset(prismaMock);
 });
 
 // creates and exports a deep mock of the prisma client (including nested object properties)
-const prisma = mockDeep<PrismaClient>();
-export default prisma;
+export const prismaMock = mockDeep<PrismaClient>();
